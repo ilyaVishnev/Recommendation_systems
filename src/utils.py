b5082d72a592f98f18fc8bd3c7024e76f36a331d
Ilia Vishnevskii <Ilia_Vishnevskii@epam.com>
2023-07-01 19:49:23 +0300

    l5

--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,5 @@ def prefilter_items(data_train, item_features, take_n_popular=5000):
 
     popularity = data_train.groupby('item_id')['quantity'].sum().reset_index()
     popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
-    top_5000 = popularity.sort_values('n_sold', ascending=False).head(take_n_popular)
-    return top_5000
+    top_5000 = popularity.sort_values('n_sold', ascending=False).head(take_n_popular).item_id.tolist()
+    return data_train.loc[data_train['item_id'].isin(top_5000)]
